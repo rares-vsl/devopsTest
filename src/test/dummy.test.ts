1116287d30c748a503cbfb55754dd52f0723e8ab
rares vasiliu <rv.vasiliu@gmail.com>
2025-09-24 12:25:12 +0200

    test: fix string
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/test/dummy.test.ts
+++ src/test/dummy.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 
 describe("dummy test", () => {
-  it("should pass", () => {
+  it("should pass always", () => {
     expect(1 + 1).toBe(2);
   });
 });
